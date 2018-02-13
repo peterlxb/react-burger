01841af85d63f89a7834be0005cbd320be16896b
peterlxb <peterlxb@gmail.com>
2018-02-13 11:06:56 +0800

    Add BurgerBuilder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,17 +6,26 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
   // demo from the video
 
+  let transformedIngredients = Object.keys(props.ingredients) //return arrays ['salad','cheese',...]
+    .map(igKey => { //igKey => salad ,cheese ....
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey}/>;
+      });
+    })
+    .reduce((arr,el) => {
+      return arr.concat(el);
+    },[]);
+
+    if(transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients</p>
+    };
+
   // const transformedIngredients = Object.keys(props.ingredients) //return arrays ['salad','cheese',...]
-  //   .map(igKey => { //igKey => salad ,cheese ....
-  //     return [...Array(props.ingredients[igKey])].map((_, i) => {
-  //       return <BurgerIngredient key={igKey + i} type={igKey}/>;
-  //     });
+  //   .map((igKey, i)=> { //igKey => salad ,cheese ....
+  //     return <BurgerIngredient key={igKey + i} type={igKey}/>;
   //   });
 
-  const transformedIngredients = Object.keys(props.ingredients) //return arrays ['salad','cheese',...]
-    .map((igKey, i)=> { //igKey => salad ,cheese ....
-      return <BurgerIngredient key={props.ingredients[igKey] + i} type={igKey}/>;
-    });
+  console.log(transformedIngredients);
 
   return (
     <div className="Burger">
