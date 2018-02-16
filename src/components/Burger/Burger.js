20dc578ebef7895f81e1285416fe049e675ce85c
peterlxb <peterlxb@gmail.com>
2018-02-16 12:58:37 +0800

    add prices

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,13 @@ import './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  // demo from the video
 
   let transformedIngredients = Object.keys(props.ingredients) //return arrays ['salad','cheese',...]
     .map(igKey => { //igKey => salad ,cheese ....
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey}/>;
       });
-    })
+    }) //addition method to avoid emypt []
     .reduce((arr,el) => {
       return arr.concat(el);
     },[]);
