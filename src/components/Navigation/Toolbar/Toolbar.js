dfc8102a763caf38776e40163c0096402213ae14
peterlxb <peterlxb@gmail.com>
2018-02-20 16:02:45 +0800

    Add toolbar and logo image

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Toolbar.css';
+import Logo from '../../Logo/Logo';
+
+const toolbar = (props) => (
+  <header className="Toolbar">
+    <div>MEUN</div>
+    <Logo />
+    <nav>
+      ...
+    </nav>
+  </header>
+);
+
+export default toolbar;
