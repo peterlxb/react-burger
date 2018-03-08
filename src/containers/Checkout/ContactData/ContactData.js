0324b25a62fd439e1b324498be20e2329e902121
peterlxb <peterlxb@gmail.com>
2018-03-08 11:47:23 +0800

    Add custom forms

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,37 +2,108 @@ import React,{ Component } from 'react';
 import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 import './ContactData.css';
 
 class ContactData extends Component {
-  state = {
-    name:'',
-    email:'',
-    address:{
-      street:'',
-      postalCode:''
-    },
-    loading:false
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      orderForm: {
+          name: {
+            elementType:'input',
+            elementConfig: {
+              type:"text",
+              placeholder:"Your Name"
+            },
+            value:'',
+            validation:{
+              required:true
+            }
+          },
+          street:{
+            elementType:'input',
+            elementConfig: {
+              type:"input",
+              placeholder:"Street"
+            },
+            value:'',
+            validation:{
+              required:true
+            }
+          },
+          zipCode:{
+            elementType:'input',
+            elementConfig: {
+              type:"text",
+              placeholder:"ZipCode"
+            },
+            value:'',
+            validation:{
+              required:true
+            }
+          },
+          country:{
+            elementType:'input',
+            elementConfig: {
+              type:"text",
+              placeholder:"Country"
+            },
+            value:'',
+            validation:{
+              required:true
+            }
+          },
+          email:{
+            elementType:'input',
+            elementConfig: {
+              type:"email",
+              placeholder:"Your Email"
+            },
+            value:'',
+            validation:{
+              required:true
+            }
+          },
+          deliveryMethod:{
+            elementType:'select',
+            elementConfig: {
+              options:[
+                {value:'fastest',displayValue:'Fastest'},
+                {value:'cheapest',displayValue:'Cheapest'}
+              ]
+            },
+            value:''
+          }
+      },
+      loading:false
+    }
+    //this.handlerChange = this.handlerChange.bind(this);
+  }
+
+  inputChangeHandler = (event,inputIdentifier) => {
+    const updatedOrderForm = {...this.state.orderForm};
+    const updatedOrderElement = {...updatedOrderForm[inputIdentifier]};
+    updatedOrderElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedOrderElement;
+    this.setState({orderForm:updatedOrderForm});
   }
 
   OrderHandler = (event) => {
     event.preventDefault();
     console.log(this.props.ingredients);
     this.setState({loading:true});
-    //alert('keep going!');
+
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
+    console.log(formData);
     const order = {
       ingredients:this.props.ingredients,
       price: this.props.price,
-      custom: {
-        name: 'Peter liu',
-        address:{
-          street:'Teststreet 1',
-          zipCode:'42311',
-          country:'China'
-        },
-        email:'test@test.com'
-      },
-      deliveryMethod:"fastest",
+      orderData:formData,
       ordertime:new Date(),
     }
 
@@ -48,13 +119,26 @@ class ContactData extends Component {
   }
 
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id:key,
+        config:this.state.orderForm[key]
+      })
+    }
 
     let form = (
       <form>
-        <input className="Input" type="text" name="name" placeholder="Your Name" />
-        <input className="Input" type="email" name="email" placeholder="Your Email" />
-        <input className="Input" type="text" name="street" placeholder="Your Street" />
-        <input className="Input" type="text" name="postal" placeholder="Your Postal" />
+
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            changed={(event) => this.inputChangeHandler(event,formElement.id)}
+          />
+        ))}
         <Button btnType="Success" clicked={this.OrderHandler}>ORDER</Button>
       </form>
     );
