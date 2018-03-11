90b5b459d0292c099cb1c4c8fbd7258437410f6a
peterlxb <peterlxb@gmail.com>
2018-03-11 19:45:15 +0800

    Add middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware,combineReducers ,compose} from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reducer from './store/reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+const logger = store => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(logger))
+)
+
 
 const app = (
   <Provider store={store}>
